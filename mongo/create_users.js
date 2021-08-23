7d44c997e2f6ed42a8e40e12570ae0980967ad17
Felipe Cardoso <felipepassoscardoso@gmail.com>
2021-08-23 19:33:56 -0300

    Fix compose

--- mongo/create_users.js
+++ mongo/create_users.js
@@ -1,26 +1,26 @@
 db.createUser(
-        {
-            user: "animon_processing",
-            pwd: "animonproc",
-            roles: [
-                {
-                    role: "readWrite",
-                    db: "animon_db"
-                }
-            ]
-        }
+    {
+        user: "animon_processing",
+        pwd: "animonproc",
+        roles: [
+            {
+                role: "readWrite",
+                db: "animon_db"
+            }
+        ]
+    }
 );
 
 db.createUser(
-        {
-            user: "animon_application",
-            pwd: "animonapp",
-            roles: [
-                {
-                    role: "readWrite",
-                    db: "animon_db"
-                }
-            ]
-        }
+    {
+        user: "animon_application",
+        pwd: "animonapp",
+        roles: [
+            {
+                role: "readWrite",
+                db: "animon_db"
+            }
+        ]
+    }
 );
 
